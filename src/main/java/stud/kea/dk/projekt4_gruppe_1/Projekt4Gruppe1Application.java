78cb414841e2d162eb3a40371484c9a1a602c509
Martin Poulsen <mapo0004@stud.kea.dk>
2024-04-08 08:36:40 +0200

    test

---
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/Projekt4Gruppe1Application.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.projekt4_gruppe_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt4Gruppe1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt4Gruppe1Application.class, args);
+    }
+
+}

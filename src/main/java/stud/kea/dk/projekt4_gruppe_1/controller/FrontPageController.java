f9db41bb4fdf932d4d4830bbfdb754809278c214
Viggo2102 <viktor.rasmussen@hotmail.com>
2024-04-10 10:27:28 +0200

    Background images, and some styling for that.

--- src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/FrontPageController.java
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/FrontPageController.java
@@ -1,4 +1,4 @@
-package stud.kea.dk.projekt4_gruppe_1.Controller;
+package stud.kea.dk.projekt4_gruppe_1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

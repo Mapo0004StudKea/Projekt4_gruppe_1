95a4c54e298073de96fff5d2b6bf906a65ae7adb
Viggo2102 <viktor.rasmussen@hotmail.com>
2024-04-10 13:13:30 +0200

    createUserHtml forbundet med Controller.

--- src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/FrontPageController.java
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/FrontPageController.java
@@ -2,6 +2,13 @@ package stud.kea.dk.projekt4_gruppe_1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import stud.kea.dk.projekt4_gruppe_1.Model.User;
+import stud.kea.dk.projekt4_gruppe_1.Repository.UserRepository;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class FrontPageController {
@@ -14,4 +21,24 @@ public class FrontPageController {
     public String frontPage() {
         return "Frontpage";
     }
+
+    @GetMapping("/createUser")
+    public String visCreate() {
+        return "createUser";
+    }
+
+    @PostMapping("/createUser") //Den her er taget fra muskikData, der mangler det sidste som først kan laves når databasen er i gang. Se musikdata projektet.
+    public String createUser(
+            @RequestParam("name") String name,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("email") String email,
+            @RequestParam("date") String dateOfBirth
+    ) {
+        LocalDate date = LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        /*User user = new User(name, lastName, email, dateOfBirth);
+        UserRepository.create(user);*/
+        return "redirect:/FrontPage";
+    }
 }
+

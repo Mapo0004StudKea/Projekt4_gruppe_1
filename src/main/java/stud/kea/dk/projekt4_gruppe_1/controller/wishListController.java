ac0ff9ed7137e9c0f070001ae436e038647c75a7
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-04-11 12:15:15 +0200

    prøver at udprinte wishlist

--- src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/wishListController.java
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/wishListController.java
@@ -2,12 +2,14 @@ package stud.kea.dk.projekt4_gruppe_1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import stud.kea.dk.projekt4_gruppe_1.Model.WishList;
 import stud.kea.dk.projekt4_gruppe_1.Repository.WishListRepository;
 
+import java.util.List;
 
 
 @Controller
@@ -17,7 +19,9 @@ public class wishListController {
     public WishListRepository wishListRepository;
 
     @GetMapping("/wishListSide")
-    public String wishListSide(){
+    public String wishListSide(Model model){
+        List<WishList> wishLists = wishListRepository.getWishList();
+        model.addAttribute("wishlist", wishLists);
         return "wishListSide";
     }
 

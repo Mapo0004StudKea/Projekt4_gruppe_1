ef6de0d97c5f31df810dab7f610b7baadffb09ff
Martin Poulsen <mapo0004@stud.kea.dk>
2024-04-11 11:03:28 +0200

    Autowired wishlist repo to wishlist controller

--- src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/wishListController.java
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/wishListController.java
@@ -1,12 +1,16 @@
 package stud.kea.dk.projekt4_gruppe_1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import stud.kea.dk.projekt4_gruppe_1.Repository.WishListRepository;
 
 @Controller
 public class wishListController {
 
+    @Autowired
+    public WishListRepository wishListRepository;
 
     @GetMapping("wishListSide")
     public String wishListSide(){

2acad25ccfd67056834cb2800fd59d7df2bdbec9
ViggoBeck <vibe0002@stud.kea.dk>
2024-04-16 11:26:55 +0200

    Style on login-forside

--- src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/FrontPageController.java
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/FrontPageController.java
@@ -1,4 +1,4 @@
-package stud.kea.dk.projekt4_gruppe_1.controller;
+package stud.kea.dk.projekt4_gruppe_1.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;

3ab1585805a5c20893a62b71740d1ddc1e4638ee
ViggoBeck <vibe0002@stud.kea.dk>
2024-04-17 10:03:57 +0200

    Small changes to

--- src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/ProductController.java
+++ src/main/java/stud/kea/dk/projekt4_gruppe_1/controller/ProductController.java
@@ -1,4 +1,4 @@
-package stud.kea.dk.projekt4_gruppe_1.controller;
+package stud.kea.dk.projekt4_gruppe_1.Controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
